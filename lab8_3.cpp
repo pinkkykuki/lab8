3797f134587ebe781377c56af94e54c912f85409
pinkkykuki <panpink1509@gmail.com>
2025-12-19 00:32:38 +0700

    Update lab8_3.cpp

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -11,3 +11,45 @@
 "Brook"
 "Jinbe"
 */
+#include <iostream>
+using namespace std;
+
+
+
+int main() {
+        int age,height,bt;
+        cout<<"Enter your age: ";
+        cin>>age;
+        if (age<=25) {
+            cout<<"Enter your height: ";
+            cin>>height; 
+            if (height<100) {
+                cout<<"Your character = Chopper";
+            }   
+            else if (height<180) {
+                cout<<"Your character = Usopp";
+            } else {
+                cout<<"Enter your bounty: ";
+                cin>>bt;
+                if (bt>1100000000) {
+                    cout<<"Your character = Zoro";
+                } else {
+                    cout<<"Your character = Sanji";
+                }
+            }
+
+        } else if (age>60) {
+            cout<<"Your character = Brook";
+        }
+        else {
+            cout<<"Enter your bounty: ";
+            cin>>bt;
+            if (bt>500000000) {
+                cout<<"Your character = Jinbe";
+            } else {
+                cout<<"Your character = Franky";
+            }
+        }
+        
+
+}
